2ca8811a76e3dcf4dd20b7880df9984a17111a71
Artem <akirillov@itps-russia.ru>
2023-08-08 03:34:30 +0500

    Настроена работа с моделью Lesson через Generics

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,10 @@
+from django.urls import path
+
 from main.apps import MainConfig
 from rest_framework.routers import DefaultRouter
 
-from main.views import CourseViewSet
+from main.views import CourseViewSet, LessonCreateAPIView, LessonListAPIView, LessonRetrieveAPIView, \
+    lessonUpdateAPIView, LessonDestroyAPIView
 
 app_name = MainConfig.name
 
@@ -9,5 +12,9 @@ router = DefaultRouter()
 router.register(r'courses', CourseViewSet, basename='courses')
 
 urlpatterns = [
-
+    path('lesson/create/', LessonCreateAPIView.as_view(), name='lesson_create'),
+    path('lesson/', LessonListAPIView.as_view(), name='lesson_list'),
+    path('lesson/<int:pk>/', LessonRetrieveAPIView.as_view(), name='lesson_detail'),
+    path('lesson/update/<int:pk>/', lessonUpdateAPIView.as_view(), name='lesson_update'),
+    path('lesson/delete/<int:pk>/', LessonDestroyAPIView.as_view(), name='lesson_delete'),
 ] + router.urls
