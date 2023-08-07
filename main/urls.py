25f1b916c5170ec9ceb820d6535b7f2b33b1ca89
Artem <akirillov@itps-russia.ru>
2023-08-08 03:17:14 +0500

    Настроена работа с моделью Course через ViewSet

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,13 @@
 from main.apps import MainConfig
+from rest_framework.routers import DefaultRouter
+
+from main.views import CourseViewSet
 
 app_name = MainConfig.name
 
+router = DefaultRouter()
+router.register(r'courses', CourseViewSet, basename='courses')
+
 urlpatterns = [
 
-]
+] + router.urls
--- main/views.py
+++ main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+
+from main.models import Course
+from main.serializers import CourseSerializer
+
 
 # Create your views here.
+class CourseViewSet(viewsets.ModelViewSet):
+    serializer_class = CourseSerializer
+    queryset = Course.objects.all()
